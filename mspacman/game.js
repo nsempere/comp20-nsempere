f98c0784efb3c1a91ddba68c6892f4a2e7746fd6
Nick Sempere <nick.sempere@sbcglobal.net>
2015-03-04 00:13:52 -0500

    implemented drawing code

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,28 @@
-// Replace this with your code...
+/*
+ *
+ *		Created By: Nicolas Sempere
+ *		Date Created: 2 March 2015
+ *
+ */
+
+function draw_arena(context, arena)
+{
+
+	context.drawImage(arena, 315, 0 /*465, 120, 10, 10, 465, 120*/); 
+ 	
+}
+ 
+function init() 
+{
+
+ 	var pac_man_canvas = document.getElementById("game_canvas");
+ 	var context = pac_man_canvas.getContext('2d');
+
+ 	var arena = new Image();
+ 
+ 	arena.addEventListener("onload", draw_arena(context, arena), false);
+	arena.src = 'pacman10-hp-sprite.png';
+
+ 	
+}
+
