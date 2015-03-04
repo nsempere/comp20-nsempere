e6f21e49797eaec25025f696886e2bc8f964c5ca
Nick Sempere <nick.sempere@sbcglobal.net>
2015-03-04 00:57:00 -0500

    commented

--- mspacman/game.js
+++ mspacman/game.js
@@ -13,12 +13,16 @@ function init()
  	var context = pac_man_canvas.getContext('2d');
 
  	var arena = new Image();
- 	arena.src = 'pacman10-hp-sprite.png';
-
+ 	var mspacman = new Image();
+ 	
  	arena.onLoad = function(){
 
+ 		//drawing arena
  		context.drawImage(arena.src, 315, 0, 465, 120, 10, 10, 465, 120); 
+ 		//drawing ms pac man at some spot in the arena
+ 		context.drawImage(mspacman.src, 80, 5, 15, 15, 100, 100, 15, 15);
 	};
- 	
+ 	arena.src = 'pacman10-hp-sprite.png';
+
 }
 
