eb453845228e161260fa61882ad7196272463ebd
Nick Sempere <nick.sempere@sbcglobal.net>
2015-03-12 00:51:58 -0400

    Prepared lab6 for debugging

--- messages/lab.js
+++ messages/lab.js
@@ -7,30 +7,32 @@
 
  function parse() {
 
+ 		var infoFeed = document.getElementById('messages');
 
  		request = new XMLHttpRequest();
  		request.open("GET", "data.json", true);
-
- 		request.onreadystatechange = post_data();
-
+ 		request.onreadystatechange = post_data(infoFeed);
  		request.send();
 
 
  }
 
- function post_data() 
+ function post_data(output) 
  {
 
+ 		var contents = "";
 
- 		if (request.readyState == 4 && request.status == 200) 
- 		{
+ 		/* Only do so if request got a successful return */
+ 		if (request.readyState == 4 && request.status == 200) {
 
  			data = JSON.parse(request.responseText);
 
- 			for (var i = 0; i < data.length; i++) 
- 			{
- 				data[i]
- 			};
+ 			/* creates a new <p> element for every element in the JSON-parsed array of objects */
+ 			for (var i = 0; i < data.length; i++) {
+ 				contents += "<p>" + data[i].content + " " + data[i].username + "</p>";
+ 			}
  		}
+ 		/* HTML element to be modified get new <p> tags */
+ 		output.innerHTML = contents;
  }
 
