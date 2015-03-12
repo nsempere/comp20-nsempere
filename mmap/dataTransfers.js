c93ab19be211692fffdda246966f34819f989c5f
Nick Sempere <nick.sempere@sbcglobal.net>
2015-03-12 19:48:07 -0400

    Still testing geolocation

--- mmap/dataTransfers.js
+++ mmap/dataTransfers.js
@@ -8,16 +8,34 @@
 
 var login = "nsempere"; //FIND THE ONE THAT MING SENT YOU
 
+function fetchCoords(position) 
+{
+		console.log("In fetchCoords");
+		var pos = "login=" + login + 
+			      "&lat=" + position.coords.latitude + 
+			      "&lng" + position.coords.longitude;
+		/*document.getElementById("test").innerHTML =*/ console.log(pos);
+}
+
+function err(position)
+{
+
+	console.log("error loading your coordinates");
+}
+
 function findMyLocation()
 {
 		if (navigator.geolocation) {
-			position = navigator.geolocation.getCurrentPosition();
-			var pos = "login=" + login + 
-					  "&lat=" + position.coords.latitude + 
-					  "&lng" + position.coords.longitude;
-			document.getElementById("test").innerHTML = "<p>" + pos + "</pos>";
+			console.log("geolocation is good. Must be fetchCoords")
+			navigator.geolocation.getCurrentPosition(fetchCoords, err);
+			console.log("done ...");
+		}
+		else {
+			document.getElementById("test").innerHTML="<p> GEOLOCATION NOT SUPPORTED HERE </p>";
+			console.log("geolocation failed")
 		}
 }
+
 
 /*function sendMyLocation()
 {
