6608caf0b69aeb5b867b46fe66c764bd444c4c4d
Nick Sempere <nicksempere@sbcglobal.net>
2016-10-02 23:44:32 -0400

    Cleaned up trivial files and did some mild refactoring

--- mmap/dataTransfers.js
+++ mmap/dataTransfers.js
@@ -3,15 +3,15 @@
  *		Created by: Nicolas Sempere
  *		Date Created: 11 March 2015
  *
+ *    NOTE: This applet is no longer functional as the resource it 
+ *          makes requests to has been taken offline.
  */
-
-
+var URI = " https://afternoon-lake-2860.herokuapp.com/sendLocation";
 var login = "KendallRumfelt";
 var map;
 var myPos;
 
-function findMyLocation()
-{
+function findMyLocation(){
 		if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(fetchCoords, err);
 		}
@@ -22,15 +22,12 @@ function findMyLocation()
 
 
 /* Helper functions for findMyLocation() */
-function err(position)
-{
-
+function err(position) {
 		var obj = document.getElementById("mapCanvas");
 		obj.innerHTML = "<p class = 'otherFeedBack'> We couldn't find your position </p>";
 }
 
-function fetchCoords(position) 
-{
+function fetchCoords(position) {
 		var pos = "login=" + login + 
 		      "&lat=" + position.coords.latitude + 
 		      "&lng=" + position.coords.longitude;
@@ -40,18 +37,14 @@ function fetchCoords(position)
 }
 
 
-function mapMyself(lat, lng)
-{
-
+function mapMyself(lat, lng) {
 		myPos = new google.maps.LatLng(lat, lng);
 		ISolemnlySwearIAmUpToNoGood(myPos);
 }
 
 /* Had to make the reference. This function initilaizes a map object and displays it on the page. */
-function ISolemnlySwearIAmUpToNoGood(myPos) 
-{
-
-
+function ISolemnlySwearIAmUpToNoGood(myPos) {
+	var marker;
 	document.getElementById("magic").innerHTML = "The Real Maurauder's Maps";
 
 	myDetails = {
@@ -61,11 +54,9 @@ function ISolemnlySwearIAmUpToNoGood(myPos)
 				};
 
 	map = new google.maps.Map(document.getElementById("mapCanvas"), myDetails);
-
-	var marker = new google.maps.Marker({ position: myPos, title: login, icon: "legend-of-zelda.png"});
+	marker = new google.maps.Marker({ position: myPos, title: login, icon: "legend-of-zelda.png"});
 	marker.setMap(map);
 	google.maps.event.addListener(marker, 'click', function(){
- 
 			var myInfoWindow = new google.maps.InfoWindow();
 			myInfoWindow.setContent(marker.title);
 			myInfoWindow.open(map, marker);
@@ -73,12 +64,9 @@ function ISolemnlySwearIAmUpToNoGood(myPos)
 }
 
 
-function sendMyLocation(position)
-{
-
+function sendMyLocation(position) {
 	request = new XMLHttpRequest();
-	//var URI = "http://secret-about-box.herokuapp.com/sendLocation";
-	var URI = " https://afternoon-lake-2860.herokuapp.com/sendLocation";
+	
 	request.open("POST", URI, true);
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	request.onreadystatechange = parseLocationFeedback;
@@ -88,41 +76,36 @@ function sendMyLocation(position)
 }
 
 
-function parseLocationFeedback()
-{
+function parseLocationFeedback() {
 		if 	(request.readyState == 4 &&	request.status == 200) {
 				var peers = JSON.parse(request.response);
 				displayOtherUsers(peers);
 		}
-
 }
 
-function displayOtherUsers(peers) 
-{
+function displayOtherUsers(peers) {
 
 		for (var i = 0; i < peers.length; i++) {
 				addMarker(peers[i]);
 		}
 }
 
-function addMarker(peer)
-{
-		var pos = new google.maps.LatLng(peer.lat, peer.lng);
-		var peerMarker = new google.maps.Marker({position: pos, title: peer.login});
+function addMarker(peer) {
+		var pos, peerMarker, distance;
+		
+		pos = new google.maps.LatLng(peer.lat, peer.lng);
+		peerMarker = new google.maps.Marker({position: pos, title: peer.login});
 		peerMarker.setMap(map);
-
-		var distance = haversine(peer);
+		distance = haversine(peer);
 
 		google.maps.event.addListener(peerMarker, 'click', function(){
 				var peerInfoWindow = new google.maps.InfoWindow();
-
 				peerInfoWindow.setContent("Name:   " + peerMarker.title + "  Distance from you:   " + distance + " miles");
 				peerInfoWindow.open(map, peerMarker);
 		});
 }
 
-function haversine(peer)
-{
+function haversine(peer) {
 		var dlat = ((peer.lat - myPos.k) * ((Math.PI) / 180));
 		var dlon = ((peer.lng - myPos.D) * ((Math.PI) / 180));
 		var mylat = (myPos.k * (Math.PI/180));
@@ -134,7 +117,5 @@ function haversine(peer)
 				  (Math.cos(mylat) * Math.cos(mylng));
 
 		var arctan = Math.atan(Math.sqrt(val1), Math.sqrt(1 - val1)) * 2;
-		var distance = earthRadius * arctan;
-		return distance;
-
+		return earthRadius * arctan;
 }
